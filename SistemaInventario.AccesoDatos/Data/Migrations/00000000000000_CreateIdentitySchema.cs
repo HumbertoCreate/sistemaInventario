e3782709be45f63c7697b4fa22e3310451ace4c9
HumbertoCreate <avalosgalvezjorge@gmail.com>
2024-03-13 08:08:47 -0600

    Patron de repositorio

--- SistemaInventario.AccesoDatos/Data/Migrations/00000000000000_CreateIdentitySchema.cs
+++ SistemaInventario.AccesoDatos/Data/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Migrations;
 using System;
 
-namespace SistemaInventario.Data.Migrations
+namespace SistemaInventario.AccesoDatos.Data.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
---
+++ SistemaInventario.AccesoDatos/Repositorio/IRepositorio/IRepositorio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio.IRepositorio
+{
+    public interface IRepositorio<T> where T : class
+    {
+        Task<T> obtener(int id);
+        Task<IEnumerable<T>> ObtenerTodos(
+            Expression<Func<T, bool>> filtro = null,
+            Func<IQueryable<T>, IQueryable<T>> orderBy = null,
+            string incluirPropiedades = null,
+            bool isTracking = true
+            );
+        Task<T> ObtenerPrimeto(
+            Expression<Func<T, bool>> filtro = null,
+            string incluirPropiedades = null,
+            bool isTracking = true
+            );
+
+        Task Agregar(T entidad);
+        void Remover(T entidad);
+        void RemoverRango(IEnumerable<T> entidad);
+    }
+}
