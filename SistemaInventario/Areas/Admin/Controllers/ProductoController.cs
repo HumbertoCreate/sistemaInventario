dcb299898f272269d666eb6a0ad891453024152e
HumbertoCreate <avalosgalvezjorge@gmail.com>
2024-04-26 10:55:42 -0600

    CRUD Productos Completo

--- SistemaInventario/Areas/Admin/Controllers/ProductoController.cs
+++ SistemaInventario/Areas/Admin/Controllers/ProductoController.cs
@@ -10,23 +10,26 @@ namespace SistemaInventario.Areas.Admin.Controllers
     public class ProductoController : Controller
     {
         private readonly IUnidadTrabajo _unidadTrabajo;
-        public ProductoController(IUnidadTrabajo unidadTrabajo)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductoController(IUnidadTrabajo unidadTrabajo, IWebHostEnvironment webHostEnvironment)
         {
             _unidadTrabajo = unidadTrabajo;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
         {
             return View();
         }
-
+        //Metodo get del Upsert
         public async Task<IActionResult> Upsert(int? id)
         {
             ProductoVM productoVM = new ProductoVM()
             {
                 Producto = new Producto(),
                 CategoriaLista = _unidadTrabajo.Producto.ObtenerTodosDropdownList("Categoria"),
-                MarcaLista = _unidadTrabajo.Producto.ObtenerTodosDropdownList("Marca")
+                MarcaLista = _unidadTrabajo.Producto.ObtenerTodosDropdownList("Marca"),
+                PadreLista = _unidadTrabajo.Producto.ObtenerTodosDropdownList("Producto")
             };
             if(id == null)
             {
@@ -47,6 +50,94 @@ namespace SistemaInventario.Areas.Admin.Controllers
 
 
         #region API
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>Upsert(ProductoVM productoVM)
+        {
+            if(ModelState.IsValid)
+            {
+                // Definimos la variable para obtener los archivos desde el formulario
+                // En este caso es solo la imagen
+                var files = HttpContext.Request.Form.Files;
+                // Definir una variable para construir la ruta del directorio WWWRoot
+                string webRootPath = _webHostEnvironment.WebRootPath;
+                if(productoVM.Producto.Id == 0)
+                {
+                    //Crear un nuevo producto
+                    //Definimo la ruta completa de donde se guardara la imagen
+                    string upload = webRootPath + DS.ImagenRuta;
+                    //Creamos un id unico de la imagen
+                    string fileName = Guid.NewGuid().ToString();
+                    //Crear la variable con el tipo de archivo de imagen (Extencion)
+                    string extension = Path.GetExtension(files[0].FileName);
+                    //Habilitemos el manejo del streaming de los archivos
+                    using (var fileStream = new FileStream(
+                        Path.Combine(upload,fileName + extension),
+                        FileMode.Create
+                        ))
+                    {
+                        //Copiamos el archivo de la memoria del navegador a la 
+                        //carpeta del servidor
+                        files[0].CopyTo(fileStream);
+                    }
+                    // Asignamos el nombre de la imagen del producto hacia su registro
+                    productoVM.Producto.ImagenUrl = fileName + extension;
+                    // Preparamos al modelo para ser guardado
+                    await _unidadTrabajo.Producto.Agregar(productoVM.Producto);
+                }
+                else
+                {
+                    // Actualizar el producto existente
+                    // Hacemos la consulta del registro a modificar
+                    var objProducto = await _unidadTrabajo.Producto
+                        .ObtenerPrimero(p => p.Id == productoVM.Producto.Id
+                        , isTracking:false);
+                    // Saber si el usuario eligio otra imagen
+                    if(files.Count > 0)
+                    {
+                        //Definimo la ruta completa de donde se guardara la imagen
+                        string upload = webRootPath + DS.ImagenRuta;
+                        //Creamos un id unico de la imagen
+                        string fileName = Guid.NewGuid().ToString();
+                        //Crear la variable con el tipo de archivo de imagen (Extencion)
+                        string extension = Path.GetExtension(files[0].FileName);
+                        //Borrar la imagen anterior
+                        var anteriorFile = Path.Combine(upload, objProducto.ImagenUrl);
+                        //Verificamos si la imagen existe
+                        if (System.IO.File.Exists(anteriorFile))
+                        {
+                            System.IO.File.Delete(anteriorFile);
+                        }
+                        using (var fileStream = new FileStream(
+                            Path.Combine(upload, fileName + extension),
+                            FileMode.Create
+                            ))
+                            {
+                                //Copiamos el archivo de la memoria del navegador a la 
+                                //carpeta del servidor
+                                files[0].CopyTo(fileStream);
+                            }
+                        //Asignamos la nueva imagen al registro del producto
+                        productoVM.Producto.ImagenUrl = fileName + extension;
+                    }
+                    // Si no se escogio otra imagen
+                    else
+                    {
+                        productoVM.Producto.ImagenUrl = objProducto.ImagenUrl;
+                    }
+                    _unidadTrabajo.Producto.Actualizar(productoVM.Producto);
+                }
+                TempData[DS.Exitosa] = "Producto Registrado con Exito";
+                await _unidadTrabajo.Guardar();
+                return View("Index");
+            }
+            // Si el modelState es Invalido
+            TempData[DS.Error] = "Algo anda Mal";
+            productoVM.CategoriaLista = _unidadTrabajo.Producto.ObtenerTodosDropdownList("Categoria");
+            productoVM.MarcaLista = _unidadTrabajo.Producto.ObtenerTodosDropdownList("Marca");
+            productoVM.PadreLista = _unidadTrabajo.Producto.ObtenerTodosDropdownList("Producto");
+            return View(productoVM);
+        }
         [HttpGet]
         public async Task<IActionResult> ObtenerTodos()
         {
@@ -62,6 +153,13 @@ namespace SistemaInventario.Areas.Admin.Controllers
             if (productoDb == null)
             {
                 return Json(new { success = false, message = "Error al Borrar Producto" });
+            }
+            //Eliminamos la imagen
+            string upload = _webHostEnvironment.WebRootPath + DS.ImagenRuta;
+            var anteriorfile = Path.Combine(upload, productoDb.ImagenUrl);
+            if (System.IO.File.Exists(anteriorfile))
+            {
+                System.IO.File.Delete(anteriorfile);
             }
             _unidadTrabajo.Producto.Remover(productoDb);
             await _unidadTrabajo.Guardar();
